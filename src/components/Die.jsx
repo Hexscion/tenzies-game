6e8f9c491190411bc2a8414c8222e3f177e0fa2e
Aldrick Rasquinha <aldrickrasquinha@gmail.com>
2023-12-12 16:36:33 +0530

    Update Die.jsx

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,3 +1,10 @@
+import dice1 from "../assets/dice1.svg"
+import dice2 from "../assets/dice2.svg"
+import dice3 from "../assets/dice3.svg"
+import dice4 from "../assets/dice4.svg"
+import dice5 from "../assets/dice5.svg"
+import dice6 from "../assets/dice6.svg"
+
 export default function Die( {value, isHeld, holdDice} ) {
 
     const styles = {
@@ -6,7 +13,7 @@ export default function Die( {value, isHeld, holdDice} ) {
 
     return (
         <div className={`die-face`} onClick={holdDice}>
-            <img src={`/src/assets/dice${value}.svg`} style={styles} alt={`dice${value}`} />
+            <img src={`/src/assets/dice${value}`} style={styles} alt={`dice${value}`} />
         </div>
     )
 }
